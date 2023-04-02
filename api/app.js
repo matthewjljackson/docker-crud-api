248586fddaeafd9f50ad791ee50a4c8968fd4b55
matthewjljackson <matthewjljackson@gmail.com>
2023-04-02 22:06:10 +0100

    feat: setup docker compose

--- api/app.js
+++ api/app.js
@@ -2,20 +2,21 @@ const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'new_schema',
-  password: 'password',
-});
-
 const app = express();
 
 app.use(cors());
 
-app.get('/', (_req, res) => {
+app.get('/', async (_req, res) => {
+  const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    database: 'books',
+    user: 'root',
+    password: 'password',
+  });
+
   db.query('SELECT * FROM Review', (err, results) => {
     console.log({ err, results });
+
     if (err) res.status(500).json(err);
 
     res.status(200).json(results);
